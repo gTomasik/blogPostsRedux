e25d422229f1534d03059c4ddd286063a54f7e4e
gTomasik <grzesiek315@gmail.com>
2019-08-01 16:33:02 +0200

    memoiz form lodash

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -8,10 +8,7 @@ class UserHeader extends React.Component {
     this.props.fetchUser(this.props.userId)
   }
   render (){
-
-    const user = this.props.users.find(
-      user=> user.id === this.props.userId
-    )
+    const { user } = this.props
 
     if (!user){
       return null
@@ -22,7 +19,9 @@ class UserHeader extends React.Component {
     )
   }
 }
-const mapStateToProps = (state) => {
-  return { users: state.users }
+const mapStateToProps = (state, ownProps) => {
+  return { user: state.users.find(
+    user=> user.id === ownProps.userId
+  )}
 }
 export default connect(mapStateToProps,{ fetchUser })(UserHeader)
